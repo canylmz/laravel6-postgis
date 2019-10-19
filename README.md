ca433c841dc60c6a63a271974fd8c012515469aa
Can YILMAZ <canylmz6134@gmail.com>
2019-10-19 19:23:42 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# Laravel 6 PostGIS
+This package implemented for calculate or check the distance between point and other database points.
+
+## Installation
+
+[PHP](https://php.net) >=7.2 and [Laravel](http://laravel.com) 6.0 are required.
+
+the package used [Laravel postgis extension](https://github.com/njbarrett/laravel-postgis) to deal with postgres database points in laravel,
+so if need more details or how to enable postgis extension in php see previous link.
+
+To get the latest version of Laravel 6 PostGIS, simply require the project using [Composer](https://getcomposer.org):
+
+```bash
+composer require canylmz/laravel6-postgis
+```
+
+## Usage
+
+1 . First of all use `Postgis` trait in your model
+```PHP
+<?php
+
+namespace App;
+
+use Canylmz\Postgis\Postgis;
+use Illuminate\Database\Eloquent\Model;
+
+class UserLocation extends Model
+{
+    use Postgis;
+}
+```
+
+2 . By default package assume that the name of the point column is `location` if you want to change it override `location` variable on your model
+```PHP
+protected $location = "my_column";
+```
+
+3 . Also By default package assume that the unit of distance is `meter` if you want to change it override `unit` variable on your model
+```PHP
+ protected $unit = "km"; //units avialble [mile, km, meter]
+```
+
+### Functions
+
+#### 1. withDistance
+get the distance between point and other points
+```PHP
+UserLocation::withDistance(new Point($atitude,$longitude))
+            ->with("user")
+            ->whereIn("user_id", $users)
+            ->get();
+```
+
+#### 2. whereDistance
+check the distance between a point and other points in database
+```PHP
+       UserLocation::whereDistance(new Point($atitude,$longitude), ">", 50)
+            ->with("user")
+            ->whereIn("user_id", $users)
+            ->get();
+```
---
+++ src/Postgis.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Canylmz\Postgis;
+
+use Illuminate\Database\Eloquent\Builder;
+use Phaza\LaravelPostgis\Eloquent\PostgisTrait;
+use Phaza\LaravelPostgis\Geometries\Point;
+
+trait Postgis
+{
+    use PostgisTrait;
+
+    /**
+     * @param Builder $query
+     * @param Point $location
+     * @return Builder
+     */
+    public function scopeWithDistance(Builder $query, $location)
+    {
+        $classQuery = $query->getQuery();
+
+        if ($classQuery && !$classQuery->columns) {
+            $query->select([$classQuery->from . '.*']);
+        }
+
+        if ($location) {
+            if ($location instanceof Point) {
+                $longitude = $location->getLng();
+                $latitude = $location->getLat();
+            } else {
+                list($longitude, $latitude) = explode(",", $location);
+            }
+
+            $division = $this->getDivisionFactor();
+
+            $q = "ST_Distance({$this->getLocationColumn()},ST_Point({$longitude},{$latitude}))/{$division}";
+        } else {
+            $q = "0";
+        }
+
+        return $query->selectSub($q, 'distance');
+    }
+
+    /**
+     * @param Builder $query
+     * @param Point $location
+     * @param float $operator
+     * @param float $units
+     * @return Builder
+     */
+    public function scopeWhereDistance(Builder $query, $location, $operator, $units)
+    {
+        $classQuery = $query->getQuery();
+
+        if ($classQuery && !$classQuery->columns) {
+            $query->select([$classQuery->from . '.*']);
+        }
+
+        if ($location) {
+            if ($location instanceof Point) {
+                $longitude = $location->getLng();
+                $latitude = $location->getLat();
+            } else {
+                list($latitude, $longitude) = $location;
+            }
+
+            $q = "ST_Distance({$this->getLocationColumn()},ST_Point({$longitude},{$latitude}))";
+        } else {
+            $q = "0";
+        }
+
+        return $query->whereRaw("$q {$operator} {$units}");
+    }
+
+    private function getLocationColumn()
+    {
+        $column = 'location';
+
+        if (property_exists($this, 'location') && $this->location) {
+            $column = $this->location;
+        }
+
+        return $this->getTable() . '.' . $column;
+    }
+
+    private function getDivisionFactor()
+    {
+        $division = 1;
+
+        if (property_exists($this, 'unit') && $this->unit == "mile") {
+            $division = 0.000621371;
+        } elseif (property_exists($this, 'unit') && $this->unit == "km") {
+            $division = 1000;
+        }
+
+        return $division;
+    }
+}
